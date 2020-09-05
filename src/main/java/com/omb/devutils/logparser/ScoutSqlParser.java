c71368f0fc0c362ae19245d05817cb4af25a8f4b
Marco Dörfliger <choppus@gmail.com>
2020-09-05 13:12:34 +0200

    Fixed scout sql parser

--- src/main/java/com/omb/devutils/logparser/ScoutSqlParser.java
+++ src/main/java/com/omb/devutils/logparser/ScoutSqlParser.java
@@ -10,43 +10,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ScoutSqlParser {
-	
-	private static Pattern REGEX = Pattern.compile("^IN  (?<bind>:\\w+) => \\? \\[(?:BIGINT|INTEGER|VARCHAR) (?<value>.*)\\]$");
+
+	private static Pattern REGEX = Pattern
+			.compile("^IN  (?<bind>:\\w+) => \\? \\[(?:BIGINT|INTEGER|VARCHAR|BOOLEAN) (?<value>\\w+)\\]$");
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sql = new StringBuilder();
-		Map<String,String> binds = new HashMap<>();
+		Map<String, String> binds = new HashMap<>();
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			System.out.println("Paste SQL:");
-	        String s;
-	        do {
-	        	s = br.readLine();
-	        	if ("*** UNPARSED ***".equals(s)) {
-	        		sql.deleteCharAt(sql.length()-1);
-	        		continue;
-	        	}
-	        	
-	        	Matcher matcher = REGEX.matcher(s);
-	        	if (matcher.matches()) {
-	        		binds.put(matcher.group("bind"), matcher.group("value"));
-	        		continue;
-	        	}
-	        	
-	        	sql.append(s).append('\n');
-	        } while (s != null && !s.isEmpty());
-		}
-		
-		for (Entry<String, String> entry : binds.entrySet()) {
-			int i = 0;
-			while(true) {
-				i = sql.indexOf(entry.getKey(), i);
-				if (i == -1) {
-					break;
+			String s;
+			do {
+				s = br.readLine();
+				if ("*** UNPARSED ***".equals(s)) {
+					sql.deleteCharAt(sql.length() - 1);
+					continue;
 				}
-				sql.replace(i, i+entry.getKey().length(), entry.getValue());
+
+				Matcher matcher = REGEX.matcher(s);
+				if (matcher.matches()) {
+					String bind = matcher.group("bind");
+					String value = matcher.group("value");
+					binds.put(bind, value);
+
+				} else if (binds.isEmpty()) {
+					sql.append(s).append('\n');
+				}
+			} while (s != null && !s.isEmpty());
+		}
+
+		for (Entry<String, String> entry : binds.entrySet()) {
+			String bind = entry.getKey();
+			String value = entry.getValue();
+			for (int i = 0; (i = sql.indexOf(bind, i)) != -1;) {
+				String replacement = "/*" + bind + "*/" + value;
+				sql.replace(i, i + bind.length(), replacement);
+				i += replacement.length();
 			}
 		}
 		System.out.println(sql.toString());
 	}
-	
+
 }
